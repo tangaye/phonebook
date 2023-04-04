b68f6736b4cdf2e32afd91a29603854ca3cb2341
Nathan Tangaye Siafa <nathan@kwagei.com>
2023-04-04 22:30:38 +0000

    added feature to create contacts and wrapped functions in an object

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Version 2 of a Phone book application I once built
 ## Requirements
 
 -   [x] There should be a way to view contacts
--   [ ] There should be a way to create contacts
+-   [x] There should be a way to create contacts
+-   [ ] There should be a way to upload images for contacts
 -   [ ] There should be a way to edit contacts
 -   [ ] There should be a way to delete contacts
 -   [ ] There should be a way to search contacts
--- app.js
+++ app.js
@@ -1,24 +1,95 @@
-const formEl = document.querySelector(".form-container");
-const newContactEl = document.querySelector(".new-contact");
-const contactsEl = document.querySelector(".contacts-container");
+const phonebook = {
+	newEl: document.querySelector(".phonebook-new"),
+	listEl: document.querySelector(".phonebook-list"),
+	formEl: document.querySelector(".phonebook-form"),
+	data: {
+		name: document.querySelector(".form-name"),
+		email: document.querySelector(".form-email"),
+	},
 
-document.addEventListener("click", function (event) {
-	const target = event.target;
-	const classes = Array.from(target.classList);
+	contacts: [
+		{ name: "Mary Doe", email: "blama@gmail.com" },
+		{ name: "Konah Doe", email: "konah@gmail.com" },
+	],
 
-	if (classes.includes("new-contact")) {
-		toggleForm();
-		toggleNewContactTitle();
-	}
-});
+	store: function () {
+		const name = this.data.name.value;
+		const email = this.data.email.value;
 
-function toggleForm() {
-	contactsEl.classList.toggle("hidden");
-	formEl.classList.toggle("hidden");
-}
+		if (name.length <= 0 || email.length <= 0) return;
 
-function toggleNewContactTitle() {
-	formEl.classList.contains("hidden")
-		? (newContactEl.innerHTML = "➕ Add Contact")
-		: (newContactEl.innerHTML = "Close Form");
-}
+		this.contacts.push({ name: name, email: email });
+
+		this.render();
+
+		this.toggleForm();
+
+		this.clearForm();
+	},
+
+	render: function () {
+		this.listEl.innerHTML = "";
+
+		for (let i = 0; i < this.contacts.length; i++) {
+			this.listEl.insertAdjacentHTML(
+				"beforeend",
+				`
+				<li class="list-item">
+					<span class="list-image">
+						<img height="200" src="./avatar2.png" />
+					</span>
+					<span class="list-info">
+						<p class="list-name">${this.contacts[i].name}</p>
+						<span class="list-email">${this.contacts[i].email}</span>
+					</span>
+					<span class="list-actions">
+						<span class="edit-item">✏️</span>
+						<span class="delete-item">❌</span>
+					</span>
+				</li>
+			`
+			);
+		}
+	},
+
+	toggleForm: function () {
+		this.listEl.classList.toggle("hidden");
+		this.formEl.classList.toggle("hidden");
+
+		this.toggleTitle();
+	},
+
+	clearForm: function () {
+		this.data.name.value = "";
+		this.data.email.value = "";
+	},
+
+	toggleTitle: function () {
+		this.formEl.classList.contains("hidden")
+			? (this.newEl.innerHTML = "➕ Add Contact")
+			: (this.newEl.innerHTML = "✖️ Close Form");
+	},
+
+	handleClick: function (event) {
+		const target = event.target;
+		const classes = Array.from(target.classList);
+
+		if (classes.includes("phonebook-new")) {
+			this.toggleForm();
+			this.toggleTitle();
+		}
+
+		if (classes.includes("form-save")) this.store();
+	},
+
+	registerHandlers: function () {
+		document.addEventListener("click", this.handleClick.bind(this));
+	},
+
+	init: function () {
+		this.render();
+		this.registerHandlers();
+	},
+};
+
+phonebook.init();
