eab39a3b72d43f908f03307d110e460d13e0f5a8
Nathan Tangaye Siafa <nathan@kwagei.com>
2023-04-05 08:52:22 +0000

    added photo feature

--- app.js
+++ app.js
@@ -2,23 +2,45 @@ const phonebook = {
 	newEl: document.querySelector(".phonebook-new"),
 	listEl: document.querySelector(".phonebook-list"),
 	formEl: document.querySelector(".phonebook-form"),
-	data: {
-		name: document.querySelector(".form-name"),
-		email: document.querySelector(".form-email"),
-	},
+	nameEl: document.querySelector(".phonebook-name"),
+	photoEl: document.querySelector(".phonebook-photo"),
+	photoPreviewEl: document.querySelector(".phonebook-photo--preview"),
+	orangeNumberEl: document.querySelector(".phonebook-number--orange"),
+	lonestarNumberEl: document.querySelector(".phonebook-number--lonestar"),
+	photoSrc: "",
 
 	contacts: [
-		{ name: "Mary Doe", email: "blama@gmail.com" },
-		{ name: "Konah Doe", email: "konah@gmail.com" },
+		{
+			photo: "",
+			name: "Mary Doe",
+			orangeNumber: "0778909090",
+			lonestarNumber: "0886787990",
+		},
+		{
+			photo: "",
+			name: "Konah Doe",
+			orangeNumber: "0778909090",
+			lonestarNumber: "0886787990",
+		},
 	],
 
 	store: function () {
-		const name = this.data.name.value;
-		const email = this.data.email.value;
+		const name = this.nameEl.value;
+		const orange = this.orangeNumberEl.value;
+		const lonestar = this.lonestarNumberEl.value;
+		const photo = this.photoSrc;
 
-		if (name.length <= 0 || email.length <= 0) return;
+		const invalid =
+			name.length <= 0 || orange.length <= 0 || lonestar.length <= 0;
 
-		this.contacts.push({ name: name, email: email });
+		if (invalid) return;
+
+		this.contacts.push({
+			photo: photo,
+			name: name,
+			orangeNumber: orange,
+			lonestarNumber: lonestar,
+		});
 
 		this.render();
 
@@ -31,16 +53,20 @@ const phonebook = {
 		this.listEl.innerHTML = "";
 
 		for (let i = 0; i < this.contacts.length; i++) {
+			const contact = this.contacts[i];
+			const imageSrc =
+				contact.photo.length > 0 ? contact.photo : "./avatar2.png";
+
 			this.listEl.insertAdjacentHTML(
 				"beforeend",
 				`
 				<li class="list-item">
 					<span class="list-image">
-						<img height="200" src="./avatar2.png" />
+						<img height="200" src="${imageSrc}" />
 					</span>
 					<span class="list-info">
-						<p class="list-name">${this.contacts[i].name}</p>
-						<span class="list-email">${this.contacts[i].email}</span>
+						<p class="list-name">${contact.name}</p>
+						<span class="list-numbers">${contact.orangeNumber} / ${contact.lonestarNumber}</span>
 					</span>
 					<span class="list-actions">
 						<span class="edit-item">✏️</span>
@@ -57,20 +83,25 @@ const phonebook = {
 		this.formEl.classList.toggle("hidden");
 
 		this.toggleTitle();
+
+		this.clearForm();
 	},
 
 	clearForm: function () {
-		this.data.name.value = "";
-		this.data.email.value = "";
+		this.nameEl.value = "";
+		this.orangeNumberEl.value = "";
+		this.lonestarNumberEl.value = "";
+		this.photoPreviewEl.innerHTML = "";
+		this.photoEl.value = "";
 	},
 
 	toggleTitle: function () {
 		this.formEl.classList.contains("hidden")
-			? (this.newEl.innerHTML = "➕ Add Contact")
-			: (this.newEl.innerHTML = "✖️ Close Form");
+			? (this.newEl.innerHTML = "➕")
+			: (this.newEl.innerHTML = "✖️");
 	},
 
-	handleClick: function (event) {
+	handleClickEvent: function (event) {
 		const target = event.target;
 		const classes = Array.from(target.classList);
 
@@ -79,11 +110,24 @@ const phonebook = {
 			this.toggleTitle();
 		}
 
-		if (classes.includes("form-save")) this.store();
+		if (classes.includes("phonebook-save")) this.store();
+	},
+
+	handleChangeEvent: function (event) {
+		const files = this.photoEl.files;
+
+		if (files.length === 0) return;
+
+		const photo = URL.createObjectURL(files[0]);
+
+		this.photoSrc = photo;
+
+		this.photoPreviewEl.innerHTML = `<img src="${photo}">`;
 	},
 
 	registerHandlers: function () {
-		document.addEventListener("click", this.handleClick.bind(this));
+		document.addEventListener("click", this.handleClickEvent.bind(this));
+		document.addEventListener("change", this.handleChangeEvent.bind(this));
 	},
 
 	init: function () {
---
+++ phonebook.js
@@ -0,0 +1,21 @@
+//TODO: There should be a way to store contacts
+
+//TODO: There should be a way to get all contacts
+
+// TODO: There shoudld be a way get a contact
+
+//TODO: There should be a way to add contacts
+
+//TODO: There should be a way to delete a contact
+
+//TODO: There should be a way to search for contacts
+
+//TODO: There should be a way to edit contacts
+
+//TODO: There should be a way to upload images when creating contacts
+
+//TODO: There should be a way to paginate contacts
+
+//TODO: There should be a way to persist contacts to localStorage
+
+//TODO: There should be a way to retrieve contacts from localStorage
