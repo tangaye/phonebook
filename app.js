df706b2640be23b5ca1f18cf6091d9ed55975452
Nathan Tangaye Siafa <nathan@kwagei.com>
2023-04-15 11:03:33 +0000

    implemeted update feature

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const phonebook = {
 	newEl: document.querySelector(".phonebook-new"),
 	listEl: document.querySelector(".phonebook-list"),
 	formEl: document.querySelector(".phonebook-form"),
-	nameEl: document.querySelector(".phonebook-name"),
 	photoEl: document.querySelector(".phonebook-photo"),
 	searchEl: document.querySelector(".phonebook-search"),
 	phonebookBackEl: document.querySelector(".phonebook-back"),
 	phonebookInfoEl: document.querySelector(".phonebook-info"),
+	phonebookSaveEl: document.querySelector(".phonebook-save"),
+	phonebookUpdateEl: document.querySelector(".phonebook-update"),
 	phonebookHeaderEl: document.querySelector(".phonebook-header"),
 	photoPreviewEl: document.querySelector(".phonebook-photo--preview"),
-	orangeNumberEl: document.querySelector(".phonebook-number--orange"),
-	lonestarNumberEl: document.querySelector(".phonebook-number--lonestar"),
 	views: {
 		current: "list-contacts",
 		list: "list-contacts",
@@ -19,11 +18,20 @@ const phonebook = {
 		edit: "edit-contact",
 	},
 
-	photoSrc: "",
+	formData: {
+		name: document.querySelector(".phonebook-name"),
+		orange: document.querySelector(".phonebook-number--orange"),
+		lonestar: document.querySelector(".phonebook-number--lonestar"),
+		email: document.querySelector(".phonebook-email"),
+		address: document.querySelector(".phonebook-address"),
+		dob: document.querySelector(".phonebook-dob"),
+		details: document.querySelector(".phonebook-details"),
+		photoSrc: "",
+	},
 
 	contacts: [],
 
-	remove: function (id) {
+	remove(id) {
 		const index = this.contacts.findIndex((contact) => contact.id === id);
 
 		if (index >= 0) {
@@ -32,36 +40,43 @@ const phonebook = {
 		}
 	},
 
-	store: function () {
-		const contact = {
-			id: this.generateId(),
-			name: this.nameEl.value,
-			orange: this.orangeNumberEl.value,
-			lonestar: this.lonestarNumberEl.value,
-			photo: this.photoSrc,
-			address: "",
-			dob: "",
-			details: "",
-			email: "",
-		};
+	store() {
+		const contact = this.getFormData();
+		contact.id = this.generateId();
 
 		if (this.valid(contact)) {
 			this.contacts.push(contact);
 
-			this.renderAll(this.contacts);
-
-			this.toggleForm();
-
 			this.clearForm();
+			this.toggleForm();
+			this.renderList();
+			this.renderAll(this.contacts);
 		}
 	},
 
-	get: function (id) {
-		return this.contacts.find((contact) => contact.id === id);
+	update(id, data) {
+		const contact = this.contacts.find((contact) => contact.id === id);
+
+		if (contact) {
+			contact.name = data.name;
+			contact.address = data.address;
+			contact.orange = data.orange;
+			contact.lonestar = data.lonestar;
+			contact.details = data.details;
+			contact.dob = data.dob;
+			contact.email = data.email;
+			contact.photo = data.photo;
+
+			this.toggleForm();
+			this.renderInfo();
+			this.toggleSaveBtn();
+			this.toggleUpdateBtn();
+			this.renderOne(id);
+		}
 	},
 
-	generateId: function () {
-		return Date.now();
+	get(id) {
+		return this.contacts.find((contact) => contact.id === id);
 	},
 
 	search() {
@@ -80,7 +95,11 @@ const phonebook = {
 		this.renderAll(contacts);
 	},
 
-	renderAll: function (contacts) {
+	generateId() {
+		return Date.now();
+	},
+
+	renderAll(contacts) {
 		this.listEl.innerHTML = "";
 
 		for (let i = 0; i < contacts.length; i++) {
@@ -106,12 +125,7 @@ const phonebook = {
 		}
 	},
 
-	renderOne: function (id) {
-		this.toggleList();
-		this.toggleInfo();
-		this.toggleHeader();
-		this.toggleBackBtn();
-
+	renderOne(id) {
 		const contact = this.get(id);
 
 		if (contact) {
@@ -150,17 +164,49 @@ const phonebook = {
 					</span>
 				</div>
 				<div class="info-row row-address">
-					<button class="phonebook-btn--large">Edit</button>
+					<button data-id="${
+						contact.id
+					}" class="phonebook-edit phonebook-btn--large">Edit</button>
 				</div>
 			`;
 		}
 	},
 
-	setView: function (view) {
+	renderEdit(id) {
+		const contact = this.get(id);
+		this.populateForm(contact);
+		this.toggleForm();
+		this.toggleSaveBtn();
+		this.toggleUpdateBtn();
+		this.toggleInfo();
+		this.setView(this.views.edit);
+	},
+	renderCreate() {
+		this.clearForm();
+		this.toggleForm();
+		this.toggleList();
+		this.toggleBackBtn();
+		this.toggleHeader();
+		this.setView(this.views.create);
+	},
+	renderInfo() {
+		this.toggleInfo();
+		this.setView(this.views.info);
+	},
+
+	renderList() {
+		this.toggleBackBtn();
+		this.toggleList();
+		this.toggleHeader();
+		this.renderAll(this.contacts);
+		this.setView(this.views.list);
+	},
+
+	setView(view) {
 		this.views.current = view;
 	},
 
-	valid: function (contact) {
+	valid(contact) {
 		const numbersValid =
 			contact.orange.length > 0 || contact.lonestar.length > 0;
 		const nameValid = contact.name.length > 0;
@@ -168,66 +214,112 @@ const phonebook = {
 		return numbersValid && nameValid;
 	},
 
-	getImageSrc: function (src) {
+	getImageSrc(src) {
 		return src.length > 0 ? src : "./avatar2.png";
 	},
 
-	goBack: function () {
+	goBack() {
 		switch (this.views.current) {
 			case this.views.info:
 				this.toggleInfo();
-				this.toggleBackBtn();
-				this.toggleList();
-				this.toggleHeader();
-				this.setView(this.views.list);
+				this.renderList();
 				break;
 			case this.views.create:
 				this.toggleForm();
-				this.setView(this.views.list);
+				this.renderList();
+				break;
+			case this.views.edit:
+				this.toggleForm();
+				this.toggleSaveBtn();
+				this.toggleUpdateBtn();
+				this.renderInfo();
 				break;
 		}
 	},
 
-	toggleList: function () {
+	toggleList() {
 		this.listEl.classList.toggle("hidden");
 	},
-	toggleBackBtn: function () {
+	toggleBackBtn() {
 		this.phonebookBackEl.classList.toggle("hidden");
 	},
 
-	toggleInfo: function () {
+	toggleSaveBtn() {
+		this.phonebookSaveEl.classList.toggle("hidden");
+	},
+
+	toggleUpdateBtn() {
+		this.phonebookUpdateEl.classList.toggle("hidden");
+	},
+
+	toggleInfo() {
 		this.phonebookInfoEl.classList.toggle("hidden");
 	},
 
-	toggleHeader: function () {
+	toggleHeader() {
 		this.phonebookHeaderEl.classList.toggle("hidden");
 	},
 
-	toggleForm: function () {
-		this.toggleList();
-		this.toggleBackBtn();
-		this.toggleHeader();
-
+	toggleForm() {
 		this.formEl.classList.toggle("hidden");
-
-		this.clearForm();
 	},
 
-	clearForm: function () {
-		this.nameEl.value = "";
-		this.orangeNumberEl.value = "";
-		this.lonestarNumberEl.value = "";
+	clearForm() {
+		this.formData.name.value = "";
+		this.formData.address.value = "";
+		this.formData.email.value = "";
+		this.formData.lonestar.value = "";
+		this.formData.orange.value = "";
+		this.formData.details.value = "";
+		this.formData.dob.value = "";
 		this.photoPreviewEl.innerHTML = "";
 		this.photoEl.value = "";
 	},
 
-	handleClickEvent: function (event) {
+	populateForm(contact) {
+		this.formData.name.value = contact.name;
+		this.formData.address.value = contact.address;
+		this.formData.email.value = contact.email;
+		this.formData.lonestar.value = contact.lonestar;
+		this.formData.orange.value = contact.orange;
+		this.formData.details.value = contact.details;
+		this.formData.dob.valueAsDate = new Date(contact.dob);
+		this.phonebookUpdateEl.dataset.id = contact.id;
+		if (contact.photo) this.previewPhoto(contact.photo);
+	},
+
+	getFormData() {
+		return {
+			name: this.formData.name.value,
+			address: this.formData.address.value,
+			email: this.formData.email.value,
+			orange: this.formData.orange.value,
+			lonestar: this.formData.lonestar.value,
+			details: this.formData.details.value,
+			dob: new Date(this.formData.dob.value).toDateString(),
+			photo: this.formData.photoSrc,
+		};
+	},
+
+	previewPhoto(src) {
+		this.photoPreviewEl.innerHTML = `<img src="${src}">`;
+	},
+
+	handleClickEvent(event) {
 		const target = event.target;
 		const classList = Array.from(target.classList);
 
-		if (classList.includes("phonebook-new")) {
-			this.toggleForm();
-			this.setView(this.views.create);
+		if (classList.includes("phonebook-new")) this.renderCreate();
+
+		if (classList.includes("phonebook-edit")) {
+			const id = Number(target.dataset.id);
+			this.renderEdit(id);
+		}
+
+		if (classList.includes("phonebook-update")) {
+			const id = Number(target.dataset.id);
+			const data = this.getFormData();
+			this.update(id, data);
 		}
 
 		if (classList.includes("phonebook-save")) this.store();
@@ -237,8 +329,11 @@ const phonebook = {
 			target.closest(".list-item")
 		) {
 			const id = Number(target.closest(".list-item").dataset.id);
+			this.toggleList();
+			this.toggleHeader();
+			this.toggleBackBtn();
+			this.renderInfo();
 			this.renderOne(id);
-			this.setView(this.views.info);
 		}
 
 		if (classList.includes("delete-item")) {
@@ -249,22 +344,22 @@ const phonebook = {
 		if (classList.includes("phonebook-back")) this.goBack();
 	},
 
-	handleKeyUpEvent: function () {
+	handleKeyUpEvent() {
 		this.search();
 	},
 
-	handleChangeEvent: function () {
+	handleChangeEvent() {
 		const files = this.photoEl.files;
 		if (files.length === 0) return;
 
 		const photo = URL.createObjectURL(files[0]);
 
-		this.photoSrc = photo;
+		this.formData.photoSrc = photo;
 
-		this.photoPreviewEl.innerHTML = `<img src="${photo}">`;
+		this.previewPhoto(photo);
 	},
 
-	registerListeners: function () {
+	registerListeners() {
 		document.addEventListener("click", this.handleClickEvent.bind(this));
 		document.addEventListener("change", this.handleChangeEvent.bind(this));
 		this.searchEl.addEventListener(
@@ -273,17 +368,18 @@ const phonebook = {
 		);
 	},
 
-	init: function () {
+	init() {
 		const id = this.generateId();
+
 		const contacts = [
 			{
 				id: id,
 				name: "Blama Doe",
-				photo: "",
+				photo: "./avatar2.png",
 				orange: "0775002002",
 				lonestar: "0886767780",
 				email: "blama@example.com",
-				dob: "01/02/1993",
+				dob: new Date("01/02/1993").toDateString(),
 				details:
 					"Lorem ipsum dolor sit amet consectetur adipisicing elit.Est nobis cumque inventore accusantium iste ab optio, obcaecati delectus doloremque totam non, illum assumenda esse voluptates facilis eius perferendis eligendi omnis.",
 				address: "Barnersville",
